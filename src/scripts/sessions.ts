84aa1524e93ab2330606467b055d800c45ea7116
Esther Ogundijo <sheyiogundijo@gmail.com>
2023-01-05 21:22:23 +0100

    updates session script

--- src/scripts/sessions.ts
+++ src/scripts/sessions.ts
@@ -1,9 +1,46 @@
 import axios from "axios";
 import { differenceInHours } from "date-fns";
+import { string } from "zod";
+
+type SessionType = {
+  bookingId: string;
+  bookingRef: string;
+  date: string;
+  endsAt: string;
+  notes: string;
+  sessionId: string;
+  startsAt: string;
+  title: string;
+  userId: string;
+};
+
+//  {
+//       bookingId: "string",
+//       bookingRef: "string",
+//       date: "string",
+//       endsAt: "string",
+//       notes: "string",
+//       sessionId: "string",
+//       startsAt: "string",
+//       title: "string",
+//       userId: "string",
+//     },
 
 class Sessions {
+  selectedSessions: SessionType[] = [];
   constructor() {
     this.displaySessions();
+    this.showBookNow();
+    this.handleModal();
+  }
+
+  showBookNow() {
+    const bookNow = document.getElementById("book-now");
+    if (this.selectedSessions.length) {
+      bookNow?.classList.remove("hidden");
+    } else {
+      bookNow?.classList.add("hidden");
+    }
   }
 
   async getAllSessions(): Promise<{
@@ -21,17 +58,27 @@ class Sessions {
     }
   }
 
-  //   bookingId: "9a471128-954e-4e64-bde9-e8147015df89";
-  //   bookingRef: "string";
-  //   date: "2019-08-24";
-  //   endsAt: "14:15:22Z";
-  //   notes: "string";
-  //   sessionId: "f6567dd8-e069-418e-8893-7d22fcf12459";
-  //   startsAt: "14:15:22Z";
-  //   title: "string";
-  //   userId: "2c4a230c-5085-4924-a3e1-25fb4fc5965b";
+  handleClick(itemElement: HTMLElement, session: SessionType) {
+    const tickIcon = itemElement.parentElement?.querySelector("svg");
+    const tickContainer =
+      itemElement.parentElement?.querySelector("#tick-container");
+    tickIcon?.classList.toggle("invisible");
+    tickContainer?.classList.toggle("bg-accent");
+
+    const bookingExist = this.selectedSessions.findIndex(
+      (element) => element.bookingId === session.bookingId
+    );
+
+    if (bookingExist === -1) {
+      this.selectedSessions.push(session);
+    } else {
+      this.selectedSessions.splice(bookingExist, 1);
+    }
+    this.showBookNow();
+  }
 
   async displaySessions() {
+    const self = this;
     try {
       const { data: sessions } = await this.getAllSessions();
       const sessionListContainer = document.querySelector(
@@ -51,25 +98,18 @@ class Sessions {
         const startDate = new Date(session.startsAt);
         const endDate = new Date(session.endsAt);
 
-        const x = differenceInHours(startDate, endDate);
+        // const x = differenceInHours(startDate, endDate);
 
-        console.log({ x, startDate, endDate });
-
-        /**nOT tICKED */
-        //   <li class="flex justify-between border-b border-black_10 mt-4 pb-2">
-        //     <div class="flex items-center">
-        //       <div class="p-4 rounded-full border border-black_15 mr-4"></div>
-        //       <div>
-        //         <p class="font-bold">Photo sesh</p>
-        //         <p>1h 30m</p>
-        //       </div>
-        //     </div>
-        //     <div>Weeks day</div>
-        //   </li>;
-
-        const html = `<div class="flex items-center">
-                <div class="p-2 rounded-full mr-4 bg-accent">
-                  <svg
+        // console.log({ x, startDate, endDate });
+        const bookingExist = this.selectedSessions.findIndex(
+          (element) => element.bookingId === session.bookingId
+        );
+        const html = `
+            <div class="flex items-center">
+              <div class="p-2 rounded-full border border-black_15 mr-4 ${
+                bookingExist !== -1 ? "bg-accent" : ""
+              }" id="tick-container">
+                         <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="none"
@@ -77,27 +117,56 @@ class Sessions {
                     stroke-width="1.5"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    class="feather feather-check w-5 h-5"
+                    class="feather feather-check w-5 h-5 ${
+                      bookingExist !== -1 ? "" : "invisible"
+                    }"
                   >
                     <polyline points="20 6 9 17 4 12"></polyline>
                   </svg>
-                </div>
-                <div>
-                  <p class="font-bold">${session.title}</p>
-                  <p class="text-sm">Ref: #${session.bookingRef}</p>
-                </div>
               </div>
-              <div>Weekend</div>
-              `;
+              <div>
+                <p class="font-bold">${session.title}</p>
+                <p class="text-sm">Ref: #${session.bookingRef}</p>
+              </div>
+            </div>
+            <div>Weeks day</div>
+          `;
+        listItem.classList.add("cursor-pointer");
         listItem.innerHTML = html;
-
         sessionListContainer?.appendChild(listItem);
+        listItem.addEventListener("click", () =>
+          self.handleClick(listItem, session)
+        );
       });
-
-      console.log({ sessions });
     } catch (error) {
       console.log(error);
     }
+  }
+
+  openModal(backdrop: HTMLElement) {
+    backdrop.classList.remove("hidden");
+  }
+  closeModal(backdrop: HTMLElement) {
+    backdrop.classList.add("hidden");
+  }
+
+  handleModal() {
+    const bookBtn = document.getElementById("book-btn");
+    const backdrop = document.getElementById("booking-modal");
+    const closeBtn = document.getElementById("close-modal-btn");
+
+    bookBtn?.addEventListener("click", () => {
+      this.openModal(backdrop!);
+    });
+
+    backdrop?.addEventListener("click", (event: any) => {
+      if (event.target.id === "outer-bk-modal") {
+        this.closeModal(backdrop!);
+      }
+    });
+    closeBtn?.addEventListener("click", () => {
+      this.closeModal(backdrop!);
+    });
   }
 }
 
